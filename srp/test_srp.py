ffcb32f22a19d46ef42704b4ac596590d48101ba
Tim Martin <tim@asymptotic.co.uk>
2017-05-03 20:52:03 +0100

    Updated to support Python 3

--- srp/test_srp.py
+++ srp/test_srp.py
@@ -5,20 +5,21 @@ import os.path
 import os
 import sys
 import time
-import thread
+from six.moves import _thread
+import six
 
 this_dir = os.path.dirname( os.path.abspath(__file__) )
 
 build_dir = os.path.join( os.path.dirname(this_dir), 'build' )
 
 if not os.path.exists( build_dir ):
-    print 'Please run "python setup.py build" prior to running tests'
+    print('Please run "python setup.py build" prior to running tests')
     sys.exit(1)
 
 plat_dirs = [ d for d in os.listdir('build') if d.startswith('lib') ]
 
 if not len(plat_dirs) == 1:
-    print 'Unexpected build result... aborting'
+    print('Unexpected build result... aborting')
 
 plat_dir = os.path.join( build_dir, plat_dirs[0] )
 
@@ -31,15 +32,15 @@ import srp._pysrp as _pysrp
 import srp._ctsrp as _ctsrp
 
 
-test_g_hex = "2"
-test_n_hex = '''\
+test_g_hex = six.b("2")
+test_n_hex = six.b('''\
 AC6BDB41324A9A9BF166DE5E1389582FAF72B6651987EE07FC3192943DB56050A37329CBB4\
 A099ED8193E0757767A13DD52312AB4B03310DCD7F48A9DA04FD50E8083969EDB767B0CF60\
 95179A163AB3661A05FBD5FAAAE82918A9962F0B93B855F97993EC975EEAA80D740ADBF4FF\
 747359D041D5C33EA71D281E446B14773BCA97B43A23FB801676BD207A436C6481F1D2B907\
 8717461A5B9D32E688F87748544523B524B0D57D5EA77A2775D2ECFA032CFBDBF52FB37861\
 60279004E57AE6AF874E7303CE53299CCC041C7BC308D82A5698F3A8D0C38271AE35F8E9DB\
-FBB694B5C803D89F7AE435DE236D525F54759B65E372FCD68EF20FA7111F9E4AFF73'''
+FBB694B5C803D89F7AE435DE236D525F54759B65E372FCD68EF20FA7111F9E4AFF73''')
 
 
 class SRPTests( unittest.TestCase ):
@@ -49,8 +50,8 @@ class SRPTests( unittest.TestCase ):
         Verifier                       = v_mod.Verifier
         create_salted_verification_key = g_mod.create_salted_verification_key
 
-        username = 'testuser'
-        password = 'testpassword'
+        username = six.b('testuser')
+        password = six.b('testpassword')
 
         _s, _v = create_salted_verification_key( username, password, hash_alg, ng_type, n_hex, g_hex )
 
@@ -147,11 +148,11 @@ class SRPTests( unittest.TestCase ):
         def verf_len(mod, val):
             with self.assertRaises(ValueError) as ctx:
                 mod.User('uname', 'pwd', bytes_a=val)
-            self.assertIn('bytes_a', ctx.exception.message)
+            self.assertIn('bytes_a', str(ctx.exception) )
 
             with self.assertRaises(ValueError) as ctx:
                 mod.Verifier('uname', random31, random31, random31, bytes_b=val)
-            self.assertIn('bytes_b', ctx.exception.message)
+            self.assertIn('bytes_b', str(ctx.exception) )
 
         for mod in [_ctsrp, _pysrp]:
             for val in [random31, random33]:
@@ -171,8 +172,8 @@ class SRPTests( unittest.TestCase ):
 hash_map  = { 0 : 'SHA1  ', 1 : 'SHA224', 2 : 'SHA256', 3 : 'SHA384', 4 : 'SHA512' }
 prime_map = { 0 : 1024, 1 : 2048, 2 : 4096, 3 : 8192 }
 
-username = 'testuser'
-password = 'testpassword'
+username = six.b('testuser')
+password = six.b('testpassword')
 
 NLEFT = 0
 
@@ -212,7 +213,7 @@ def performance_test( mod, hash_alg, ng_type, niter=10, nthreads=1 ):
 
     start = time.time()
     while nthreads > 1:
-        thread.start_new_thread( test_thread, () )
+        _thread.start_new_thread( test_thread, () )
         nthreads -= 1
 
     test_thread()
@@ -234,60 +235,60 @@ def get_param_str( mod, hash_alg, ng_type ):
 def param_test( mod, hash_alg, ng_type, niter=10 ):
     duration = performance_test( mod, hash_alg, ng_type, niter )
     cfg = get_param_str( mod, hash_alg, ng_type )
-    print '   ', cfg.ljust(20), '%.6f' % (duration/niter)
+    print('   ', cfg.ljust(20), '%.6f' % (duration/niter))
     return duration/niter
 
 
 def print_default_timings():
-    print '*'*60
-    print 'Default Parameter Timings:'
+    print('*'*60)
+    print('Default Parameter Timings:')
     py_time = param_test( _pysrp, srp.SHA1, srp.NG_2048 )
     ct_time = param_test( _ctsrp, srp.SHA1, srp.NG_2048 )
-    print ''
-    print 'Performance increases: '
-    print '   ctypes-module : ', py_time/ct_time
+    print('')
+    print('Performance increases: ')
+    print('   ctypes-module : ', py_time/ct_time)
 
 
 def print_performance_table():
     ng_types = [ srp.NG_1024, srp.NG_2048, srp.NG_4096, srp.NG_8192 ]
     hash_types = [ srp.SHA1, srp.SHA224, srp.SHA256, srp.SHA384, srp.SHA512 ]
 
-    print '*'*60
-    print 'Hash Algorithm vs Prime Number performance table'
-    print ''
-    print '       |',
+    print('*'*60)
+    print('Hash Algorithm vs Prime Number performance table')
+    print('')
+    six.print_('       |', end=' ')
     for ng in ng_types:
-        print ('NG_%d' % prime_map[ng]).rjust(12),
-    print ''
-    print '-'*60
+        six.print_(('NG_%d' % prime_map[ng]).rjust(12), end=' ')
+    print('')
+    print('-'*60)
 
     for hash_alg in hash_types:
 
-        print '%s |' % hash_map[hash_alg],
+        six.print_('%s |' % hash_map[hash_alg], end=' ')
         for ng in ng_types:
-            print '{0:>12f}'.format(performance_test(_ctsrp, hash_alg, ng) / 10),
-        print ''
+            six.print_('{0:>12f}'.format(performance_test(_ctsrp, hash_alg, ng) / 10), end=' ')
+        print('')
 
 
 def print_thread_performance():
-    print '*'*60
-    print 'Thread Performance Test:'
+    print('*'*60)
+    print('Thread Performance Test:')
     niter = 100
     for nthreads in range(1,11):
-        print '   Thread Count {0:>2}: {1:8f}'.format(nthreads, performance_test(_ctsrp, srp.SHA1, srp.NG_2048, niter, nthreads)/niter)
+        print('   Thread Count {0:>2}: {1:8f}'.format(nthreads, performance_test(_ctsrp, srp.SHA1, srp.NG_2048, niter, nthreads)/niter))
 
 
-print '*'*60
-print '*'
-print '* Testing Implementation'
-print '*'
+print('*'*60)
+print('*')
+print('* Testing Implementation')
+print('*')
 suite = unittest.TestLoader().loadTestsFromTestCase(SRPTests)
 unittest.TextTestRunner(verbosity=1).run(suite)
 
-print '*'*60
-print '*'
-print '* Performance Testing'
-print '*'
+print('*'*60)
+print('*')
+print('* Performance Testing')
+print('*')
 print_thread_performance()
 print_performance_table()
 print_default_timings()
