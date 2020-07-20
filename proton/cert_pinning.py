70a91948f6bb24d6d6a77d1ed6d18688f0d87aba
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-07-20 11:11:09 +0100

    Add alternative hash dicts to HTTPAdapter
    This now gives the possibility to provide the adapter an alternative hash dict, which is useful for testing purposes
    
    Also, the code now is inline with flake8

--- proton/cert_pinning.py
+++ proton/cert_pinning.py
@@ -1,7 +1,5 @@
 import base64
 import hashlib
-import json
-import os
 from ssl import DER_cert_to_PEM_cert
 
 import requests
@@ -9,6 +7,7 @@ from OpenSSL import crypto
 from requests.adapters import HTTPAdapter
 from urllib3.connectionpool import HTTPSConnectionPool
 from urllib3.poolmanager import PoolManager
+from urllib3.util.timeout import Timeout
 
 from .constants import PUBKEY_HASH_DICT
 
@@ -20,36 +19,86 @@ class TLSPinningError(requests.exceptions.SSLError):
 
 
 class TLSPinningHTTPSConnectionPool(HTTPSConnectionPool):
-    """Custom HTTPSConnectionPool that verifies the certificate for each connection"""
-    
+    """HTTPSConnectionPool that verifies the certificate for each connection"""
+    def __init__(
+        self,
+        host,
+        alt_hash_dict,
+        port=None,
+        strict=False,
+        timeout=Timeout.DEFAULT_TIMEOUT,
+        maxsize=1,
+        block=False,
+        headers=None,
+        retries=None,
+        _proxy=None,
+        _proxy_headers=None,
+        key_file=None,
+        cert_file=None,
+        cert_reqs=None,
+        key_password=None,
+        ca_certs=None,
+        ssl_version=None,
+        assert_hostname=None,
+        assert_fingerprint=None,
+        ca_cert_dir=None,
+        **conn_kw
+    ):
+        self.alt_hash_dict = alt_hash_dict
+        super(TLSPinningHTTPSConnectionPool, self).__init__(
+            host,
+            port,
+            strict,
+            timeout,
+            maxsize,
+            block,
+            headers,
+            retries,
+            _proxy,
+            _proxy_headers,
+            key_file,
+            cert_file,
+            cert_reqs,
+            key_password,
+            ca_certs,
+            ssl_version,
+            assert_hostname,
+            assert_fingerprint,
+            ca_cert_dir,
+            **conn_kw
+        )
+
     def _validate_conn(self, conn):
         r = super(TLSPinningHTTPSConnectionPool, self)._validate_conn(conn)
-        
+
         sock = conn.sock
 
         pem_certificate = self.get_certificate(sock)
 
-        if self.is_session_secure(pem_certificate, conn):
+        if self.is_session_secure(pem_certificate, conn, self.alt_hash_dict):
             return r
 
-
-    def is_session_secure(self, cert, conn):
+    def is_session_secure(self, cert, conn, alt_hash_dict):
         """Checks if connection is secure"""
-        
+
         cert_hash = self.extract_hash(cert)
 
-        if not self.validate_hash(cert_hash):
+        if not self.validate_hash(cert_hash, alt_hash_dict):
             # Also generate a report
             conn.close()
             raise TLSPinningError("Insecure connection")
-        
+
         return True
 
-
-    def validate_hash(self, cert_hash):
+    def validate_hash(self, cert_hash, alt_hash_dict):
         """Validates the hash agains a known list of hashes/pins"""
+        hash_dict = PUBKEY_HASH_DICT
+
+        if alt_hash_dict:
+            hash_dict = alt_hash_dict
+
         try:
-            PUBKEY_HASH_DICT[self.host].index(cert_hash)
+            hash_dict[self.host].index(cert_hash)
         except (ValueError, KeyError):
             return False
         else:
@@ -71,27 +120,57 @@ class TLSPinningHTTPSConnectionPool(HTTPSConnectionPool):
         pubkey = crypto.dump_publickey(crypto.FILETYPE_ASN1, pubkey_obj)
         spki_hash = hashlib.sha256(pubkey).digest()
         cert_hash = base64.b64encode(spki_hash).decode('utf-8')
-        
+
         return cert_hash
 
 
 class TLSPinningPoolManager(PoolManager):
-    """Custom PoolManager that attaches a custom HTTPSConnectionPool to a new connection pool"""
+    """
+    Custom PoolManager that attaches
+    a custom HTTPSConnectionPool to a new connection pool
+    """
+    def __init__(
+        self,
+        alt_hash_dict,
+        num_pools=10,
+        headers=None,
+        **connection_pool_kw
+    ):
+        self.alt_hash_dict = alt_hash_dict
+        super(TLSPinningPoolManager, self).__init__(
+            num_pools=10, headers=None, **connection_pool_kw
+        )
+
     def _new_pool(self, scheme, host, port, request_context):
         if scheme != 'https':
-            return super(TLSPinningPoolManager, self)._new_pool(scheme, host, port, request_context)
+            return super(TLSPinningPoolManager, self)._new_pool(
+                scheme, host, port, request_context
+                )
 
         kwargs = self.connection_pool_kw
 
-        pool = TLSPinningHTTPSConnectionPool(host, port, **kwargs)
-        
+        pool = TLSPinningHTTPSConnectionPool(
+                    host=host, port=port,
+                    alt_hash_dict=self.alt_hash_dict, **kwargs
+                )
+
         return pool
 
 
 class TLSPinningAdapter(HTTPAdapter):
     """HTTPAdapter that attaches the custom PoolManager to a session"""
-    def __init__(self):
+    def __init__(self, alt_hash_dict=None):
+        self.alt_hash_dict = alt_hash_dict
         super(TLSPinningAdapter, self).__init__()
 
-    def init_poolmanager(self, connections, maxsize, block=False, **pool_kwargs):
-        self.poolmanager = TLSPinningPoolManager(num_pools=connections, maxsize=maxsize, block=block, strict=True, **pool_kwargs)
+    def init_poolmanager(
+        self,
+        connections,
+        maxsize,
+        block=False,
+        **pool_kwargs
+    ):
+        self.poolmanager = TLSPinningPoolManager(
+            num_pools=connections, maxsize=maxsize, block=block,
+            strict=True, alt_hash_dict=self.alt_hash_dict, **pool_kwargs
+        )
