75a910151e95c513ee55736284454f376c63af45
Alexandru Cheltuitor <calexandru2018@pm.me>
2021-04-30 17:59:13 +0100

    Add new exceptions

---
+++ proton/exceptions.py
@@ -0,0 +1,29 @@
+class ProtonError(Exception):
+    def __init__(self, ret):
+        self.code = ret['Code']
+        self.error = ret['Error']
+        try:
+            self.headers = ret["Headers"]
+        except KeyError:
+            self.headers = ""
+
+        super().__init__("{}".format(self.error))
+
+
+class ProtonNetworkError(Exception):
+    def __init__(self, message, additional_context=None):
+        self.message = message
+        self.additional_context = additional_context
+        super().__init__(self.message)
+
+
+class TLSPinningError(ProtonNetworkError):
+    """TLS Pinning exception"""
+
+
+class NewConnectionError(ProtonNetworkError):
+    """Network Error"""
+
+
+class UnknownConnectionError(ProtonNetworkError):
+    """UnknownConnectionError"""
